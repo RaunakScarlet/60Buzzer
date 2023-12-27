1286e80112f81ea6f45354a5f18b6e641fdacca6
Raunak Scarlet <raunakscarlet@gmail.com>
2023-12-27 17:06:21 +0530

    made navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar";
 
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
       <>
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+          <Navbar/>
       </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from '/logo.webp'
+
+const Navbar = () => {
+  return (
+      <header className="h-20 shadow-sm flex items-center  border-red-300">
+          <nav className="flex justify-between items-center w-[90%] mx-auto">
+              <a href="/" className="">
+                  <img className="w-16 h-16 rounded-full" src={logo} alt="logo" />
+              </a>
+              <div className="flex items-center">
+                  <ul className=" sm:flex items-center gap-5 sm:gap-10 text-lg ">
+                      <li>How it works?</li>
+                      <li>Features</li>
+                      <li>About us</li>
+                  </ul>
+              </div>
+              <button className="border border-red-300 rounded text-lg">
+                  Login
+              </button>
+          </nav>
+      </header>
+  );
+}
+
+export default Navbar
